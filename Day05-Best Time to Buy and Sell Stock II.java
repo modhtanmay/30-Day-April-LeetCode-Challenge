93521bfdd9c09e11840702fe8ac329ee07582ed7
modhtanmay <39961497+modhtanmay@users.noreply.github.com>
2020-06-01 23:59:03 +0530

    Update Day05-Best Time to Buy and Sell Stock II.java

--- Day05-Best Time to Buy and Sell Stock II.java
+++ Day05-Best Time to Buy and Sell Stock II.java
@@ -22,7 +22,7 @@ Example 3:
 
 Input: [7,6,4,3,1]
 Output: 0
-Explanation: In this case, no transaction is done, i.e. max profit = 0.
+Explanation: In this case, no transaction is done, i.e. max profit = 0
  
 
 Constraints:
